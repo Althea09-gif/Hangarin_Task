089f971239879166b6f464dd95c289d88118406c
Althea09-git <202280369@psu.palawan.edu.ph>
2026-04-12 22:49:15 +0800

    Fix service worker caching

--- projectsite/static/js/serviceworker.js
+++ projectsite/static/js/serviceworker.js
@@ -1,19 +1,10 @@
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open('hangarin-cache-v1').then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/login/',
-        '/dashboard/',
-      ]);
-    })
-  );
-});
+self.addEventListener('fetch', function(event) {
+    // Only cache GET requests
+    if (event.request.method !== 'GET') return;
 
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            return caches.match(event.request);
+        })
+    );
 });
